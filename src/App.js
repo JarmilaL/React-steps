71944701ef57c9988c97b5dabebc79eb24463354
Jarmila Lenyiova <lenyiova.jarmila@gmail.com>
2024-05-21 12:15:29 +0200

    Use children in new Button comp.

--- src/App.js
+++ src/App.js
@@ -36,21 +36,30 @@ export default function App() {
           </p>
 
           <div className="buttons">
-            <button
-              style={{ backgroundColor: '#7950f2', color: '#fff' }}
+            <Button
+              textColor={'#fff'}
+              bgColor={'#7950f2'}
               onClick={handlePrevious}
             >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: '#7950f2', color: '#fff' }}
-              onClick={handleNext}
-            >
-              Next
-            </button>
+              <span>👈</span>Previous
+            </Button>
+            <Button textColor={'#fff'} bgColor={'#7950f2'} onClick={handleNext}>
+              Next <span>👉</span>
+            </Button>
           </div>
         </div>
       )}
     </>
+  );
+}
+
+function Button({ textColor, bgColor, onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
   );
 }
